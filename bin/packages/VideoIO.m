f0fa146e8ee4a3f2419a891dd16f4103b5e67260
kmisiunas <k.misiunas@gmail.com>
2016-01-08 14:01:57 +0200

    semicolons...

--- bin/packages/VideoIO.m
+++ bin/packages/VideoIO.m
@@ -17,6 +17,8 @@
                              ROI.m exported as separate package *)
 (*Version 3.0 (2015-11-14) - Allow to specify Import library (eg. Import or FFImport) *)
 (*Version 3.1 (2015-12-04) - Stable under unexpected FF stream closure  *)
+(*Version 3.2 (2016-01-08) - Migrated from importing using "Frames" -> "ImageList" for tiff stack support*)
+
 
 (* ::Plan for future::*)
 (*
@@ -31,7 +33,7 @@
 (* Package Declarations*)
 
 
-BeginPackage["VideoIO`", {"FFmpeg`", "VideoAnalysisHelpers`", "ROI`"}]
+BeginPackage["VideoIO`", {"FFmpeg`", "VideoAnalysisHelpers`", "ROI`"}];
 
 
 (* ---  Get Frame functions  --- *)
@@ -39,35 +41,35 @@ BeginPackage["VideoIO`", {"FFmpeg`", "VideoAnalysisHelpers`", "ROI`"}]
 
 VideoGet::usage = 
   "VideoGet[no_ || range_] returns a buffered video frame that was cut to ROICurrent[]. 
-  Can pass a flag \"NoBuffer\" to have unbuffered read from file."
+  Can pass a flag \"NoBuffer\" to have unbuffered read from file.";
 
 VideoGetRaw::usage = 
-  "VideoGetRaw[no_ || range_] loads a frame straight from VideoFile[] file with no processing"  
+  "VideoGetRaw[no_ || range_] loads a frame straight from VideoFile[] file with no processing";
 
 VideoFrameID::usage =
   "VideoFrameID[no_(optional)] returns id of a frame or full list of them. 
-  (if there was none set by video will give 1..VideoLength[])"
+  (if there was none set by video will give 1..VideoLength[])";
 
-VideoFile::usage = "the video file under analysis"
+VideoFile::usage = "the video file under analysis";
 
 VideoSelect::usage = 
 		"VideoSelect[file] loads the video file into analysis.
-		If file is omitted, it will bring dialogue."
+		If file is omitted, it will bring dialogue.";
 
 VideoSelect::nofile = "file does not exist.";
 
 VideoBufferAll::usgae = 
-  "VideoBufferAll[] attempts to buffer all the images in to the memory (efficiently)"
+  "VideoBufferAll[] attempts to buffer all the images in to the memory (efficiently)";
 
 VideoClearBuffer::usage =
-  "VideoClearBuffer[] clears entire buffered data"
+  "VideoClearBuffer[] clears entire buffered data";
 
 VideoLength::usage =
-  "VideoLength[] returns number of frames in buffered video"
+  "VideoLength[] returns number of frames in buffered video";
 
 VideoAddProcessRawFrame::usage = 
   "VideoAddProcessRawFrame[ func[no_Integer, img_Image] ] allows to add pre-processing step 
-  that is called each time Raw frame is processed in early stages (good for additional operations)"
+  that is called each time Raw frame is processed in early stages (good for additional operations)";
 
 (* ---  Global variables functions  --- *)
 
@@ -77,13 +79,13 @@ Options[VideoIO] = {
   "BufferBlockSize" -> 400 (*number of frames to load into memory at one time *),
   "FrameIdFromFrame" -> False (*frame top left corner has frame id *),
   "Import" -> FFImport (*function to use for importing images*)
-}
+};
 
 (* ::Section:: *)
 (*Package Implementations - Public*)
 
 
-Begin["`Private`"]
+Begin["`Private`"];
 
 
 (* ::Section:: *)
@@ -93,26 +95,26 @@ Begin["`Private`"]
 videoFile = ""; numberOfFrames = 0; framesIds = {};
 preprocessRawFrames = {}; (*types of functions to apply for pre processing raw frames*)
 
-import := OptionValue[VideoIO, "Import"]
+import := OptionValue[VideoIO, "Import"];
 
 (*private: process RAW frame - including pre-processing state*)
 VideoProcessRawFrame[no_Integer, img_Image] := (
   #[no, img] &/@ preprocessRawFrames;
   ColorConvert[ ImageTrim[img, ROICurrent[] + {{0.5, 0.5}, {-0.5, -0.5}}], "Grayscale"]
-)
+);
 
 
 VideoAddProcessRawFrame[fn_] := Module[{},
   preprocessRawFrames = DeleteDuplicates@Append[preprocessRawFrames, fn];
   Print["VideoAddProcessRawFrame: new function count is "<>ToString@Length@preprocessRawFrames]
-]
+];
 
 
-VideoFile[] := videoFile
+VideoFile[] := videoFile;
 
 VideoSelect[file_String] := 
-	If[ FileExistsQ@file, PrepareVideoInput[file],	Message[VideoSelect::nofile, file] ]
-VideoSelect[] := VideoSelect[ SystemDialogInput["FileOpen", Directory[]] ]
+	If[ FileExistsQ@file, PrepareVideoInput[file],	Message[VideoSelect::nofile, file] ];
+VideoSelect[] := VideoSelect[ SystemDialogInput["FileOpen", Directory[]] ];
 
 PrepareVideoInput[file_String] := Module[ {img, modifyFrameID},
   numberOfFrames = import[file, {"FrameCount"}];
@@ -124,13 +126,13 @@ PrepareVideoInput[file_String] := Module[ {img, modifyFrameID},
   If[ OptionValue[VideoIO, "FrameIdFromFrame"], (*try reading frame ids along the way?*)
     modifyFrameID[no_Integer, img_Image] := (framesIds[[no]] = VideoReadFrameID[img]);
     VideoAddProcessRawFrame @ modifyFrameID
-  ]
+  ];
   VideoClearBuffer[];
   FileNameTake[file] <> " has "<>ToString@numberOfFrames <> 
     " frames and is "<> ToString@dimensions[[1]] <>"x"<>ToString@dimensions[[2]]
-]
+];
 
-VideoLength[] := numberOfFrames
+VideoLength[] := numberOfFrames;
 
 
 (* Descrption:
@@ -139,23 +141,23 @@ VideoLength[] := numberOfFrames
 VideoGet[frame_Integer] := (
   If[ ! BufferContainsQ[frame] , AskBufferFor[frame] ];
   bufferVideo[[ WhichBlock[frame], frame - (WhichBlock[frame] - 1)*blockSize ]]
-)
+);
 
-VideoGet[range_?VectorQ] := VideoGet/@range
-VideoGet[frame_Integer, "NoBuffer"] := VideoProcessRawFrame[frame, #] &@ VideoGetRaw@frame
-VideoGet[range_?VectorQ, "NoBuffer"] := VideoProcessRawFrame @@ # &/@ ({#, VideoGetRaw[#]} &/@ range)
+VideoGet[range_?VectorQ] := VideoGet/@range;
+VideoGet[frame_Integer, "NoBuffer"] := VideoProcessRawFrame[frame, #] &@ VideoGetRaw@frame;
+VideoGet[range_?VectorQ, "NoBuffer"] := VideoProcessRawFrame @@ # &/@ ({#, VideoGetRaw[#]} &/@ range);
 
-VideoGetRaw[frame_Integer] := import[ VideoFile[], {"Frames", frame} ]
-VideoGetRaw[range_?VectorQ] := import[ VideoFile[], {"Frames", range} ]
+VideoGetRaw[frame_Integer] := import[ VideoFile[], {"Frames", frame} ];
+VideoGetRaw[range_?VectorQ] := import[ VideoFile[], {"Frames", range} ];
 
-VideoFrameID[] := framesIds
-VideoFrameID[no_Integer] := framesIds[[no]]
+VideoFrameID[] := framesIds;
+VideoFrameID[no_Integer] := framesIds[[no]];
 
 (* ::Section:: *)
 (*Buffering*)
 
 
-WhichBlock[frame_] := Ceiling[ (frame - 0.5)/blockSize ]
+WhichBlock[frame_] := Ceiling[ (frame - 0.5)/blockSize ];
 
 VideoClearBuffer[] := Module[ {} ,
   Clear[bufferVideo];
@@ -167,9 +169,9 @@ VideoClearBuffer[] := Module[ {} ,
   (*other stuff*)
   If[singleFrameSize*blockSize /10^6 > OptionValue[VideoIO, "BufferBlockSize"],
     Print@"Warrning: buffer too small for current ROI."  ]
-]
+];
 
-AskBufferFor[frame_] := If[ bufferVideo[[ WhichBlock @ frame ]] == {} , LoadBufferBlock[ frame ] ]
+AskBufferFor[frame_] := If[ bufferVideo[[ WhichBlock @ frame ]] == {} , LoadBufferBlock[ frame ] ];
 
 (*loads files into memory buffer. Before loading checks memory consumption*)
 LoadBufferBlock[frame_] := Module[{from, to},
@@ -179,10 +181,10 @@ LoadBufferBlock[frame_] := Module[{from, to},
   PrintTemporary @ ("Buffer: loading frame block: ["<>ToString@from<>", "<> ToString@to<>"]");
   bufferVideo[[ WhichBlock @ frame ]] = VideoGet[ Range[from, to] , "NoBuffer"] ;
   bufferVideoTimeStamp[[ WhichBlock @ frame ]] = AbsoluteTime[];
-]
+];
 
 (*MB used by the buffer*)
-BufferMemorySize[] := singleFrameSize * Total[ Length /@ bufferVideo ] / 10^6 // N
+BufferMemorySize[] := singleFrameSize * Total[ Length /@ bufferVideo ] / 10^6 // N;
 
 MakeSpaceInBuffer[] := Module[ {oldestBlockId, creationDates},
   creationDates = MapIndexed[{#2[[1]], #1} &, bufferVideoTimeStamp ] ;
@@ -191,9 +193,9 @@ MakeSpaceInBuffer[] := Module[ {oldestBlockId, creationDates},
       ToString@((oldestBlockId+1)*blockSize)<>"]");
   bufferVideo[[ oldestBlockId ]] = {};
   bufferVideoTimeStamp[[ oldestBlockId ]] = 0;
-]
+];
 
-BufferContainsQ[frame_] := bufferVideoTimeStamp[[ WhichBlock @ frame ]] > 0 
+BufferContainsQ[frame_] := bufferVideoTimeStamp[[ WhichBlock @ frame ]] > 0 ;
 
 VideoBufferAll[] := Switch[ OptionValue[VideoIO, "Import"],
   FFImport, VideoBufferAllUsingFFImport[],
@@ -208,7 +210,8 @@ VideoBufferAllUsingImport[] := Module[ {size,expectedNoOfFrames, chuncks, imageB
   Monitor[
     bufferVideo = Table[ (*parallel?*)
       (*compute on chunks of frames here*)
-      VideoProcessRawFrame @@ # &/@ Transpose[ {chuncks[[i]]  , Import[ VideoFile[] , {"Frames", chuncks[[i]]} ]} ]
+      VideoProcessRawFrame @@ # &/@ Transpose[ {chuncks[[i]]  ,
+        Import[ VideoFile[] , {"Frames", chuncks[[i]]} ]} ]
       ,
       {i, Length[chuncks]}
     ],
@@ -219,7 +222,7 @@ VideoBufferAllUsingImport[] := Module[ {size,expectedNoOfFrames, chuncks, imageB
   numberOfFrames = Total[Length /@ bufferVideo];
   framesIds = framesIds[[ ;;VideoLength[] ]]; (*shorten the list to correspond to the new buffer*)
   bufferVideoTimeStamp[[ ;; Length@bufferVideo ]] = AbsoluteTime[];
-]
+];
 
 (*loads all frames using ffmpeg! - focus on speed and stability*)
 VideoBufferAllUsingFFImport[] := Module[
@@ -250,12 +253,12 @@ VideoBufferAllUsingFFImport[] := Module[
         " frames, but FFmpeg found " <> ToString@newFrameCount , Red];
   ];
   (*do not return anything*)
-]
+];
 
 (* ::Section:: *)
 (*The End*)
 
 
-End[ ]
+End[ ];
 
-EndPackage[ ]
+EndPackage[ ];
